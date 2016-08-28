db7ebf27b0da53c3757276b8ccb950319afeec27
Vishwas <sharma.vishwas88@gmail.com>
2016-08-28 04:17:12 -0700

    Tmux and Vim editions.

--- install.sh
+++ install.sh
@@ -2,41 +2,43 @@
 INSTALLDIR=${INSTALLDIR:-"$PWD/dot-files"}
 
 ##### Set up vimified #####
-
-# Fetch vimified git repository
-cd
-if [ ! -d vimified ]; then
-    git clone git://github.com/zaiste/vimified.git
-    ln -sfn vimified/ ~/.vim
-    ln -sfn vimified/vimrc ~/.vimrc
+if [ ! -d ~/vimified ]; then
+    # Directory to set vim files.
+    VIM=~/vimified
+    # Clone vimified.
+    git clone git://github.com/zaiste/vimified.git $VIM
+    # Create pointers to vim files.
+    ln -sfn $VIM ~/.vim
+    ln -sfn $VIM/vimrc ~/.vimrc
+    # Create directory to set up dependency bundles.
+    mkdir -p $VIM/bundle
+    # Create directory to set up temporary files.
+    mkdir -p $VIM/tmp/backup
+    mkdir -p $VIM/tmp/swap
+    mkdir -p $VIM/tmp/undo
+    # Clone vundle.
+    git clone https://github.com/gmarik/vundle.git $VIM/bundle/vundle
+    # Basic package list.
+    cp $INSTALLDIR/vim/local.vimrc $VIM
+    # Install basic packages.
+    cd $VIM
+    vim +BundleInstall +qall
+    # Install solarized theme.
+    # This theme is only available after the bundle install.
+    cp $INSTALLDIR/vim/after.vimrc $VIM
+    # Move back to home folder.
+    cd
 fi
-cd vimified
-
-# Create directory to set up dependency bundles
-mkdir -p bundle
-mkdir -p tmp/backup tmp/swap tmp/undo
-
-# Setup Vundle to procure dependency bundles
-if [ ! -d bundle/vundle ]; then
-    git clone https://github.com/gmarik/vundle.git bundle/vundle
-fi
-
-# Copy *.vimrc files into vimified
-cp $INSTALLDIR/vim/local.vimrc .
-cp $INSTALLDIR/vim/after.vimrc .
-
-vim +BundleInstall +qall 2>/dev/null
 
 ##### Set up tmux #####
-
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    # Clone tpm.
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 # Copy .tmux.conf
-cd
 cp $INSTALLDIR/tmux/tmux.conf ~/.tmux.conf
 
 ##### Set up bash #####
-
-# Copy BASH files.
-cd
 cp $INSTALLDIR/bash/bash_profile ~/.bash_profile
 cp $INSTALLDIR/bash/bash_aliases ~/.bash_aliases
 source ~/.bash_profile
